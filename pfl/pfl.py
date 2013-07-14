38570197a7606569f396adc37ef6dca230d54849
studoret <studoret@fake.bzh>
2013-07-14 22:36:22 +0200

    Python code static checking.

--- pfl/pfl.py
+++ pfl/pfl.py
@@ -23,99 +23,119 @@ along with pfl.  If not, see http://www.gnu.org/licenses/.
 
 import pyo, wx
   
-from metro.metro import *
-from metro.panel import *
-from metro.mgr import *
-from pedals.panel import *
-from track.panel import *
-from track.mgr import *
+from metro.metro import Metro
+from metro.panel import MetroPanel
+from metro.mgr import MetroManager
+from pedals.panel import PedalsPanel
+from track.panel import TrackPanel
+from track.mgr import Track
+from track.mgr import TracksManager
+from utils.action import ActionCursor
 
-from utils.action import *
+SERVER = pyo.Server().boot()
+SERVER.start()
+METRO = Metro()
 
-s = pyo.Server().boot()
-s.start()
-m = Metro()
+class PanelsManager():
+    """Manage all panels
+    """
+    PEDALS_PANEL = 0
+    METRO_PANEL  = 1
+    FIRST_SOUNDTRACK_PANEL = 2
+    def __init__(self, frame):
+        self.__tracks_panel = [TrackPanel(frame)]
+        self.__current_track_id = self.__class__.METRO_PANEL
+        self.__increment = 1
+        self.__panels = [PedalsPanel(frame), MetroPanel(frame)]
+        metro_panel = self.__panels[self.__class__.METRO_PANEL]
+        control_panel = self.__panels[self.__class__.PEDALS_PANEL]
+        self.__panels.extend(self.__tracks_panel)
+        for panel in self.__panels:
+            frame.fsizer.Add(panel, 1, wx.ALL | 
+                             wx.EXPAND | 
+                             wx.ALIGN_CENTER_HORIZONTAL)
+        self.__action = ActionCursor("TRACK    ", self.TrackUp, self.TrackDown)
+        control_panel.SetTitle(0, self.__action.GetTitle())  
+        control_panel.SetSubtitle(0, self.__action.GetSubtitle())  
+        control_panel.AddManager(self)
+        self.metro_manager = MetroManager(control_panel, METRO, metro_panel)
+        self.tracks_manager = TracksManager(METRO, self, control_panel)
+        self.tracks_manager.AddTrack(Track(self.__tracks_panel[0]))
+        self.DoSelection(self.__current_track_id)
 
-class PanelManager():
-  PEDALS_PANEL = 0
-  METRO_PANEL  = 1
-  FIRST_SOUNDTRACK_PANEL = 2
-  def __init__(self, frame):
-    self.__tracksPanel = [TrackPanel(frame)]
-    self.__currentTrackId = self.__class__.METRO_PANEL
-    self.__increment = 1
-    self.__panels = [PedalsPanel(frame), MetroPanel(frame)]
-    controlPanel = self.__panels[self.__class__.PEDALS_PANEL]
-    self.__panels.extend(self.__tracksPanel)
-    for panel in self.__panels:
-      frame.fSizer.Add(panel, 1, wx.ALL | wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL)
-    self.__action = ActionCursor("TRACK    ",self.TrackUp, self.TrackDown)
-    controlPanel.SetTitle(0, self.__action.GetTitle())  
-    controlPanel.SetSubtitle(0, self.__action.GetSubtitle())  
-    controlPanel.AddManager(self)
-    self.metroManager = MetroManager(controlPanel, m, self.__panels[self.__class__.METRO_PANEL])
-    self.tracksManager = TracksManager(m, self, controlPanel)
-    self.tracksManager.AddTrack(Track(self.__tracksPanel[0]))
-    self.DoSelection(self.__currentTrackId)
-
-  def DoSelection(self, trackId):
-    self.__panels[trackId].Select()
-    if trackId == self.__class__.METRO_PANEL:
-      self.tracksManager.Deselect()
-      self.metroManager.Select()
-    else:
-      self.metroManager.Deselect()
-      self.tracksManager.Select()
-    self.tracksManager.SetCurrentTrackID(trackId - self.__class__.FIRST_SOUNDTRACK_PANEL)
+    def DoSelection(self, track_id):
+        """Deselect the previous selected panel
+        and select the new one
+        """
+        self.__panels[track_id].Select()
+        if track_id == self.__class__.METRO_PANEL:
+            self.tracks_manager.Deselect()
+            self.metro_manager.Select()
+        else:
+            self.metro_manager.Deselect()
+            self.tracks_manager.Select()
+        sound_track_id = track_id - self.__class__.FIRST_SOUNDTRACK_PANEL
+        self.tracks_manager.SetCurrentTrackID(sound_track_id)
     
-  def ChangeSelection(self):
-    oldId = self.__currentTrackId
-    self.__currentTrackId += self.__increment
-    if self.__currentTrackId == self.__class__.PEDALS_PANEL:
-      self.__currentTrackId = len(self.__panels) - 1
-    if self.__currentTrackId == len(self.__panels):
-      self.__currentTrackId = self.__class__.PEDALS_PANEL + 1
-    self.__panels[oldId].Deselect()
-    self.DoSelection(self.__currentTrackId)
+    def ChangeSelection(self):
+        """Update the __current_track_id variable
+        """
+        old_id = self.__current_track_id
+        self.__current_track_id += self.__increment
+        if self.__current_track_id == self.__class__.PEDALS_PANEL:
+            self.__current_track_id = len(self.__panels) - 1
+        if self.__current_track_id == len(self.__panels):
+            self.__current_track_id = self.__class__.PEDALS_PANEL + 1
+        self.__panels[old_id].Deselect()
+        self.DoSelection(self.__current_track_id)
 
-  def TrackUp(self):
-    self.__increment = 1
-    self.ChangeSelection()
+    def TrackUp(self):
+        """Select a new track
+        """
+        self.__increment = 1
+        self.ChangeSelection()
 
-  def TrackDown(self):
-    self.__increment = -1
-    self.ChangeSelection()
+    def TrackDown(self):
+        """Select a new track
+        """
+        self.__increment = -1
+        self.ChangeSelection()
 
-  def PedalDown(self, pedalId, keyDownCount):
-    print "pedalDown = " + str(pedalId) + " " + str(keyDownCount)
-    if pedalId == 0 and keyDownCount >= 2:
-      self.__action.Reverse()
+    def PedalDown(self, pedal_id, key_down_count):
+        """Do action on pedal down
+        """
+        print "pedalDown = " + str(pedal_id) + " " + str(key_down_count)
+        if pedal_id == 0 and key_down_count >= 2:
+            self.__action.Reverse()
 
-  def PedalUp(self, pedalId, keyDownCount):
-    print "pedalUp = " + str(pedalId) + " " + str(keyDownCount)
-    if pedalId == 0 and keyDownCount < 2:
-      self.__action.Select()
+    def PedalUp(self, pedal_id, key_down_count):
+        """Do action on pedal up
+        """
+        print "pedalUp = " + str(pedal_id) + " " + str(key_down_count)
+        if pedal_id == 0 and key_down_count < 2:
+            self.__action.Select()
 
 
 class MyFrame(wx.Frame):
-  def __init__(self, parent, title): 
-    wx.Frame.__init__(self, parent, -1, title)
-    self.fSizer = wx.BoxSizer(wx.VERTICAL) 
-    self.mgr = PanelManager(self) 
+    """The main frame
+    """
+    def __init__(self, parent, title): 
+        wx.Frame.__init__(self, parent, -1, title)
+        self.fsizer = wx.BoxSizer(wx.VERTICAL) 
+        self.mgr = PanelsManager(self) 
       
-    self.SetSizer(self.fSizer)
+        self.SetSizer(self.fsizer)
     
-    self.Centre()
-    self.Show()
-    self.fSizer.Layout()
-    wx.CallAfter(self.Fit)
+        self.Centre()
+        self.Show()
+        self.fsizer.Layout()
+        wx.CallAfter(self.Fit)
 
 # boilerplate to allow running as script directly
 if __name__ == "__main__" and __package__ is None:
- 
-  app = wx.PySimpleApp()
-  frame = MyFrame(None, 'Python Foot Looper: test metronome')
-  app.MainLoop()
-  m.StartPlayback()
+    APP = wx.PySimpleApp()
+    MyFrame(None, 'Python Foot Looper: test metronome')
+    APP.MainLoop()
+    METRO.StartPlayback()
 
 
