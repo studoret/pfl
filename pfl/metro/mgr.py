92b1bffe5b7c71c19aabb0b28c341f3e2e828f85
studoret <studoret@fake.bzh>
2013-07-20 12:05:18 +0200

    Python code static checking.

--- pfl/metro/mgr.py
+++ pfl/metro/mgr.py
@@ -2,9 +2,7 @@
 module pfl.metro.mgr
 Manager of pfl.metro.metro object .
 
-"""
-
-"""
+############
 Copyright 2013 Stephane Tudoret
 
 This file is part of pfl, a python foot looper application.
@@ -24,122 +22,122 @@ along with pfl.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import sys, os
-parent_dir =  os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(0, parent_dir)
-import pfl
+sys.path.insert(0, os.path.dirname(os.path.dirname(
+    os.path.dirname(
+      os.path.abspath(__file__)))))
+
 __package__ = str("pfl")
 del sys, os
 
-from utils.action import *
-
-from metro import *
-import time
-
-class ActionTap(Action):
-  subtitles = ["(", ")"]
-  def __init__(self, setBpmFunction):
-    self.__state = 0
-    Action.__init__(self, "TAP     ", self.__class__.subtitles[self.__state])
-    self.__begin = None
-    self.__setBpm = setBpmFunction
-
-  def Select(self):
-    if self.__begin == None:
-      self.__state = 1
-      self.__begin = time.time()
-      return
-    self.__state = 0
-    bpm = int(60 / (time.time() - self.__begin))
-    self.__setBpm(bpm)
-    self.__begin = None
-    
-  def GetSubtitle(self):
-    return self.__class__.subtitles[self.__state]
+from utils.action import ActionSwitch, ActionTap
+from utils.action import ActionMenu, ActionCursor
 
 class MetroManager():
-  def __init__(self, controlPanel, metro, metroPanel):
-    self.__metro = metro
-    self.__selected = False
-    self.__menu =  ActionMenu()
-    self.__menu.Add(ActionCursor("TEMPO   ",self.__metro.TempoUp, self.__metro.TempoDown))
-    self.__menu.Add(ActionCursor("BEAT    ",self.__metro.BeatUp, self.__metro.BeatDown))
-    self.__menu.Add(ActionCursor("VOL.    ",self.__metro.MulUp, self.__metro.MulDown))
-    self.__menu.Add(ActionTap(self.__metro.ForceTempo))
-    self.__mute = ActionSwitch(0, "SOUND ON  ", "SOUND OFF ", self.__metro.StartPlayback, self.__metro.StopPlayback)
-    self.__metro.AddMonitor(self)
-    self.__cp = controlPanel
-    self.__cp.AddManager(self)
-    self.__metroPanel = metroPanel
-    self.Refresh()
+    """ Class that monitores a metronome and the control panel
+        and manage a metro panel 
+    """
+    def __init__(self, control_panel, metro, metro_panel):
+        self.__metro = metro
+        self.__selected = False
+        self.__menu =  ActionMenu()
+        self.__menu.Add(ActionCursor("TEMPO   ",
+                                     self.__metro.TempoUp,
+                                     self.__metro.TempoDown))
+        self.__menu.Add(ActionCursor("BEAT    ",
+                                     self.__metro.BeatUp,
+                                     self.__metro.BeatDown))
+        self.__menu.Add(ActionCursor("VOL.    ",
+                                     self.__metro.MulUp,
+                                     self.__metro.MulDown))
+        self.__menu.Add(ActionTap(self.__metro.ForceTempo))
+        self.__mute = ActionSwitch(0, "SOUND ON  ", "SOUND OFF ", 
+                                   self.__metro.StartPlayback,
+                                   self.__metro.StopPlayback)
+        self.__metro.AddMonitor(self)
+        self.__cp = control_panel
+        self.__cp.AddManager(self)
+        self.__metro_panel = metro_panel
+        self.Refresh()
 
-  def Select(self):
-    self.__selected = True
-    self.__cp.SetTitle(1, self.__menu.GetTitle())  
-    self.__cp.SetSubtitle(1, self.__menu.GetSubtitle())  
-    self.__cp.SetTitle(2, self.__menu.GetActionTitle()) 
-    self.__cp.SetSubtitle(2, self.__menu.GetActionSubtitle()) 
-    self.__cp.SetTitle(3, self.__mute.GetTitle()) 
-    self.__cp.SetSubtitle(3, self.__mute.GetSubtitle())
+    def Select(self):
+        """ Select the manager 
+        """ 
+        self.__selected = True
+        self.__cp.SetTitle(1, self.__menu.GetTitle())  
+        self.__cp.SetSubtitle(1, self.__menu.GetSubtitle())  
+        self.__cp.SetTitle(2, self.__menu.GetActionTitle()) 
+        self.__cp.SetSubtitle(2, self.__menu.GetActionSubtitle()) 
+        self.__cp.SetTitle(3, self.__mute.GetTitle()) 
+        self.__cp.SetSubtitle(3, self.__mute.GetSubtitle())
 
-  def Deselect(self):
-    self.__selected = False
+    def Deselect(self):
+        """ Select the manager 
+        """ 
+        self.__selected = False
 
-  def GetCurrentAction(self):
-    return self.__actions[self.__actionIdx]
+    def PedalDown(self, pedal_id, key_down_count):
+        """ PedalDown call back 
+        """ 
+        if self.__selected == False or pedal_id == 0:
+            return
+        if pedal_id == 1:
+            if key_down_count >= 2:
+                self.__menu.Reverse()
+                self.__cp.SetSubtitle(pedal_id, self.__menu.GetSubtitle())
+            return
+        if pedal_id == 2:
+            action = self.__menu.GetCurrentAction()
+            if action != None:
+                if key_down_count >= 2:
+                    action.Reverse()
+                    self.__cp.SetSubtitle(pedal_id, action.GetSubtitle())
+            return
+        if pedal_id == 3:
+            self.__mute.Select()
+            self.__cp.SetTitle(pedal_id, self.__mute.GetTitle())
 
-  def PedalDown(self, pedalId, keyDownCount):
-    if self.__selected == False or pedalId == 0:
-      return
-    if pedalId == 1:
-      if keyDownCount >= 2:
-        self.__menu.Reverse()
-        self.__cp.SetSubtitle(pedalId, self.__menu.GetSubtitle())
-      return
-    if pedalId == 2:
-      action = self.__menu.GetCurrentAction()
-      if action != None:
-        if keyDownCount >= 2:
-          action.Reverse()
-          self.__cp.SetSubtitle(pedalId, action.GetSubtitle())
-      return
-    if pedalId == 3:
-      self.__mute.Select()
-      self.__cp.SetTitle(pedalId, self.__mute.GetTitle())
-
-  def PedalUp(self, pedalId, keyDownCount):
-    if self.__selected == False or pedalId == 0:
-      return
-    if pedalId == 1:
-      if keyDownCount < 2:
-        self.__menu.Select()
-        self.__cp.SetTitle(pedalId + 1, self.__menu.GetActionTitle())
-        self.__cp.SetSubtitle(pedalId + 1, self.__menu.GetActionSubtitle())
-      return
-    if pedalId == 2:
-      action = self.__menu.GetCurrentAction()
-      if action != None:
-        if keyDownCount < 2:
-          action.Select()
-          self.__cp.SetTitle(pedalId,action.GetTitle())
-          self.__cp.SetSubtitle(pedalId,action.GetSubtitle())
-      return
+    def PedalUp(self, pedal_id, key_down_count):
+        """ PedalUp call back 
+        """ 
+        if self.__selected == False or pedal_id == 0:
+            return
+        if pedal_id == 1:
+            if key_down_count < 2:
+                self.__menu.Select()
+                self.__cp.SetTitle(pedal_id + 1, 
+                                   self.__menu.GetActionTitle())
+                self.__cp.SetSubtitle(pedal_id + 1, 
+                                      self.__menu.GetActionSubtitle())
+            return
+        if pedal_id == 2:
+            action = self.__menu.GetCurrentAction()
+            if action != None:
+                if key_down_count < 2:
+                    action.Select()
+                    self.__cp.SetTitle(pedal_id, action.GetTitle())
+                    self.__cp.SetSubtitle(pedal_id, action.GetSubtitle())
+            return
  
-  def Refresh(self):
-    self.__metroPanel.RefreshTempo(str(self.__metro.GetTempo()))
-    self.__metroPanel.RefreshMul(str(self.__metro.GetMul()))
-    beat = self.__metro.GetBeat()
-    if beat < 10:
-      beatStr = " "+str(beat)
-    else:
-      beatStr = str(beat)
-    self.__metroPanel.RefreshBeat(beatStr)
+    def Refresh(self):
+        """ Refresh the metro panel
+        """
+        self.__metro_panel.RefreshTempo(str(self.__metro.GetTempo()))
+        self.__metro_panel.RefreshMul(str(self.__metro.GetMul()))
+        beat = self.__metro.GetBeat()
+        if beat < 10:
+            beat_str = " "+str(beat)
+        else:
+            beat_str = str(beat)
+        self.__metro_panel.RefreshBeat(beat_str)
 
-  def Tick(self):
-    tick = self.__metro.GetTick()
-    if tick == 1:
-      color = "green"
-    else:
-      color = "blue"
-    self.__metroPanel.RefreshTick(str(tick), color)
+    def Tick(self):
+        """ Tick callback
+        """
+        tick = self.__metro.GetTick()
+        if tick == 1:
+            color = "green"
+        else:
+            color = "blue"
+        self.__metro_panel.RefreshTick(str(tick), color)
       
                    
--- pfl/utils/action.py
+++ pfl/utils/action.py
@@ -22,129 +22,170 @@ You should have received a copy of the GNU General Public License
 along with pfl.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import time
+
 class Action():
-  def __init__(self, title, subtitle=""):
-    self.__title = title
-    self.__subtitle = subtitle
+    def __init__(self, title, subtitle=""):
+        self.__title = title
+        self.__subtitle = subtitle
 
-  def GetTitle(self):
-    return self.__title
+    def GetTitle(self):
+        """ Return the title
+        """
+        return self.__title
 
-  def GetSubtitle(self):
-    return self.__subtitle
+    def GetSubtitle(self):
+        """ Return the subtitle
+        """
+        return self.__subtitle
 
-  def Begin(self):
-    pass
+    def Begin(self):
+        pass
 
-  def End(self):
-    pass
+    def End(self):
+        pass
 
-  def Select(self):
-    pass
+    def Select(self):
+        pass
 
-  def Reverse(self):
-    pass
+    def Reverse(self):
+        pass
 
 class ActionLong(Action):
-  def __init__(self, title, startFunction, stopFunction):
-    Action.__init__(self, title)
-    self.__actions = [startFunction, stopFunction]
+    def __init__(self, title, startFunction, stopFunction):
+        Action.__init__(self, title)
+        self.__actions = [startFunction, stopFunction]
 
-  def Down(self):
-    self.__actions[0]()
+    def Down(self):
+        self.__actions[0]()
 
-  def Up(self):
-    self.__actions[1]()
-
-class ActionCursor(Action):
-  subtitles = {-1:u'\u21E7', 1:u'\u21E9'}
-  
-  def __init__(self, title, incFunction, decFunction):
-    self.__increment = 1
-    Action.__init__(self, title, self.__class__.subtitles[self.__increment])
-    self.actions = {-1: incFunction, 1: decFunction}
-
-  def Select(self):
-    self.actions[self.__increment]()
-
-  def Reverse(self):
-    self.__increment *= -1
-
-  def GetSubtitle(self):
-    return self.__class__.subtitles[self.__increment]
+    def Up(self):
+        self.__actions[1]()
 
 class ActionCursor(Action):
-  subtitles = {-1:u'\u21E7', 1:u'\u21E9'}
+    subtitles = {-1:u'\u21E7', 1:u'\u21E9'}
+
+    def __init__(self, title, incFunction, decFunction):
+        self.__increment = 1
+        Action.__init__(self, title, self.__class__.subtitles[self.__increment])
+        self.actions = {-1: incFunction, 1: decFunction}
+
+    def Select(self):
+        self.actions[self.__increment]()
+
+    def Reverse(self):
+        self.__increment *= -1
+
+    def GetSubtitle(self):
+        """ Return the subtitle
+        """
+        return self.__class__.subtitles[self.__increment]
+
+class ActionCursor(Action):
+    subtitles = {-1:u'\u21E7', 1:u'\u21E9'}
   
-  def __init__(self, title, incFunction, decFunction):
-    self.__increment = 1
-    Action.__init__(self, title, self.__class__.subtitles[self.__increment])
-    self.actions = {-1: incFunction, 1: decFunction}
+    def __init__(self, title, incFunction, decFunction):
+        self.__increment = 1
+        Action.__init__(self, title, self.__class__.subtitles[self.__increment])
+        self.actions = {-1: incFunction, 1: decFunction}
 
-  def Select(self):
-    self.actions[self.__increment]()
+    def Select(self):
+        self.actions[self.__increment]()
 
-  def Reverse(self):
-    self.__increment *= -1
+    def Reverse(self):
+        self.__increment *= -1
 
-  def GetSubtitle(self):
-    return self.__class__.subtitles[self.__increment]
+    def GetSubtitle(self):
+        """ Return the subtitle
+        """
+        return self.__class__.subtitles[self.__increment]
 
 
 class ActionSwitch(Action):
-  def __init__(self, state, onTitle, offTitle, onFunction, offFunction):
-    self.__titles = {0: onTitle, 1: offTitle}
-    self.__state = state
-    Action.__init__(self, self.__titles[self.__state])
-    self.__actions = {0: offFunction, 1: onFunction}
-    self.__actions[(state + 1)%2]
+    def __init__(self, state, onTitle, offTitle, onFunction, offFunction):
+        self.__titles = {0: onTitle, 1: offTitle}
+        self.__state = state
+        Action.__init__(self, self.__titles[self.__state])
+        self.__actions = {0: offFunction, 1: onFunction}
+        self.__actions[(state + 1)%2]
     
-  def Select(self):
-    self.__state += 1
-    self.__state %= 2
-    self.__actions[self.__state]()
+    def Select(self):
+        self.__state += 1
+        self.__state %= 2
+        self.__actions[self.__state]()
 
-  def GetTitle(self):
-    return self.__titles[self.__state] 
+    def GetTitle(self):
+        """ Return the title
+        """
+        return self.__titles[self.__state] 
 
 class ActionMenu(Action):
-  subtitles = {-1:u'\u21E7', 1:u'\u21E9'}
+    subtitles = {-1:u'\u21E7', 1:u'\u21E9'}
 
-  def __init__(self):
-    self.__increment = 1
-    Action.__init__(self, "MENU    ", self.__class__.subtitles[self.__increment])
-    self.__actions = []
-    self.__idx = 0
+    def __init__(self):
+        self.__increment = 1
+        Action.__init__(self, "MENU    ", self.__class__.subtitles[self.__increment])
+        self.__actions = []
+        self.__idx = 0
   
-  def Add(self, action):
-    self.__actions.append(action)
+    def Add(self, action):
+        self.__actions.append(action)
   
-  def GetCurrentAction(self):
-    if len(self.__actions) > 0:
-      return self.__actions[self.__idx]
-    else:
-      return None
+    def GetCurrentAction(self):
+        if len(self.__actions) > 0:
+            return self.__actions[self.__idx]
+        else:
+            return None
 
-  def GetActionTitle(self):
-    if len(self.__actions) > 0:
-      return self.__actions[self.__idx].GetTitle()
+    def GetActionTitle(self):
+        if len(self.__actions) > 0:
+            return self.__actions[self.__idx].GetTitle()
 
-  def GetActionSubtitle(self):
-    if len(self.__actions) > 0:
-      return self.__actions[self.__idx].GetSubtitle()
+    def GetActionSubtitle(self):
+        if len(self.__actions) > 0:
+            return self.__actions[self.__idx].GetSubtitle()
 
-  def Select(self):
-    if len(self.__actions) > 0:
-      self.__idx += self.__increment
-      if self.__idx == -1:
-        self.__idx += len(self.__actions) 
-      else:
-        if self.__idx == len(self.__actions):
-          self.__idx = 0
+    def Select(self):
+        if len(self.__actions) > 0:
+            self.__idx += self.__increment
+            if self.__idx == -1:
+                self.__idx += len(self.__actions) 
+            else:
+                if self.__idx == len(self.__actions):
+                    self.__idx = 0
 
-  def Reverse(self):
-    self.__increment *= -1
+    def Reverse(self):
+        self.__increment *= -1
 
-  def GetSubtitle(self):
-    return self.__class__.subtitles[self.__increment]
+    def GetSubtitle(self):
+        """ Return the subtitle
+        """
+        return self.__class__.subtitles[self.__increment]
     
+class ActionTap(Action):
+    """Action class used to mesure speed of taping in bpm
+    """
+    subtitles = ["(", ")"]
+    def __init__(self, bpmFunction):
+        self.__state = 0
+        Action.__init__(self, "TAP     ", 
+                        self.__class__.subtitles[self.__state])
+        self.__begin = None
+        self.__bpm_function = bpmFunction
+
+    def Select(self):
+        """ Used to start or stop mesure
+        """
+        if self.__begin == None:
+            self.__state = 1
+            self.__begin = time.time()
+            return
+        self.__state = 0
+        bpm = int(60 / (time.time() - self.__begin))
+        self.__bpm_function(bpm)
+        self.__begin = None
+    
+    def GetSubtitle(self):
+        """ Return the subtitle
+        """
+        return self.__class__.subtitles[self.__state]
